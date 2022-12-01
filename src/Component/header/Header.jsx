df8dcf3ac8b3d8c61a3a8e63e7dfd789fb1a8d29
muhammed fayis thangal <muhammedfayisthangal@gmail.com>
2022-12-01 13:04:12 +0530

    started

---
+++ src/Component/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+   <header>
+    <nav className='container'>
+        
+
+    </nav>
+   </header>
+  )
+}
+
+export default Header
