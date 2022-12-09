ca2a7a932d7d37f7336cdf7d3755ad49222ace14
muhammed fayis thangal <muhammedfayisthangal@gmail.com>
2022-12-09 05:32:16 +0530

    logo cursur pointer style added

--- src/Component/header/Header.jsx
+++ src/Component/header/Header.jsx
@@ -29,19 +29,18 @@ const Header = () => {
               </text>
             </svg>
           </div>
- <div>
-   <input type="checkbox" id="checked" />
-   <label  for="checked">
-          <svg
-            className="bar"
-           
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-          >
-            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-          </svg>
-          </label>
-         
+          <div>
+            <input type="checkbox" id="checked" />
+            <label for="checked">
+              <svg
+                className="bar"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+              </svg>
+            </label>
+
             <div id="nav-list" className="nav-list">
               <ul>
                 <li>Home</li>
